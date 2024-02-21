a66ace152161c952242b78aaf41d185568bf8f62
Mateus Felipe Araújo <brasil.mateus748@gmail.com>
2024-02-21 03:44:32 -0300

    Add block of code to the pageLoad function Fill div#content dynamically

--- src/pageload.js
+++ src/pageload.js
@@ -1,23 +1,60 @@
 import img from './img.jpg';
+import icon from './icon.jpg';
 
 export default function pageLoad() {
 
-    const title = document.createElement("h1")
-    const contentTitle = document.createTextNode("Romeo's Restaurant")
-    title.appendChild(contentTitle);
-    const element = document.getElementById("content")
-    element.appendChild(title);
-    
-    const para = document.createElement("p");
-    const node = document.createTextNode("Fanciest restaurant! \n Come and bring your family!");
-    para.appendChild(node);
-    element.appendChild(para);
+    const element = document.getElementById("content");
 
+    const title = document.createElement("h1");
+    const contentTitle = document.createTextNode("Alighieri's Pizzeria");
+    title.appendChild(contentTitle);
+    element.appendChild(title);
+    title.style.color = '-webkit-linear-gradient(#eee, #333)';
+
+    
+    
+    const h4 = document.createElement("h4");
+    const secondTitle = document.createTextNode("Fanciest pizzeria of the city!");
+    h4.appendChild(secondTitle);
+    element.appendChild(h4);
+    h4.style.margin = "20px"
+
+    const myIcon = new Image();
+    myIcon.src = icon;
+
+    element.appendChild(myIcon);
+    myIcon.style.width = '200px';
+    myIcon.style.height = '200px';
+    myIcon.style.marginBottom = '10px'
+
+    myIcon.style.border = '1px solid rgba(11, 36, 11)';
+    myIcon.style.borderRadius = '50%'
+
+    
+    const para = document.createElement('p');
+    const paraContent = document.createTextNode(`Welcome to Alighieri's – where passion meets flavor! 
+    Step into a world of delicious indulgence, where every slice tells a story of craftsmanship and love for the art of pizza-making.`);
+    para.appendChild(paraContent);
+    element.appendChild(para);
+    para.style.width = "500px";
+    para.style.margin = "0 auto";
+    
+
+    
     const myImg = new Image();
     myImg.src = img;
-
-    element.appendChild(myImg);
     
+    
+    
+
+
+    /*
+    element.appendChild(myImg);
+    myImg.style.width = '100%';
+    myImg.style.height = '100%';
+    myImg.style.margin = '20px 0px'
+    */
+
 }
 
 pageLoad();
