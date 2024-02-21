388a49638c82a290d0e8b8cbf57ea2603a87d91f
Mateus Felipe Araújo <brasil.mateus748@gmail.com>
2024-02-21 03:48:35 -0300

    Create event listener to homeBtn

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import './style.css';
 
 
 console.log("Hello World!")
-const homeBtn = document.
+const homeBtn = document.querySelector(".home")
+homeBtn.addEventListener('click', function (){
 function component() {
     const contentPage = document.querySelector("#content")
     pageLoad;
     return contentPage;
-
 }
-
 component();
+});
 
 
