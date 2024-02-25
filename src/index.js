ad9c463e59493950c908eb5aa3a34e635173c948
Mateus Felipe Araújo <brasil.mateus748@gmail.com>
2024-02-24 23:03:10 -0300

    Create Menu and About event listeners

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
-import pageLoad from './pageload.js';
+import { pageLoad, menuLoad } from './pageload.js';
 import './style.css';
 
 
 console.log("Hello World!")
 const homeBtn = document.querySelector(".home")
-homeBtn.addEventListener('click', function (){
-function component() {
+homeBtn.addEventListener('click', function() {
     const contentPage = document.querySelector("#content")
-    pageLoad;
+    pageLoad();
     return contentPage;
-}
-component();
 });
 
 
+const menuBtn = document.querySelector(".menu")
+menuBtn.addEventListener('click', function() {
+    const contentPage = document.querySelector("#content");
+    contentPage.innerHTML = ''
+   menuLoad();
+
+});
+
+
+const aboutBtn = document.querySelector(".about")
+aboutBtn.addEventListener('click', function() {
+
+})
